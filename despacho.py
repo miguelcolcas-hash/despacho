756225fc6cb1585398204f6bcdec025e0996b2de
mcolcas <miguel.colcas@gmail.com>
2026-06-10 07:37:54 -0500

    Add files via upload

--- despacho.py
+++ despacho.py
@@ -28,7 +28,18 @@ hide_github_style = """
 """
 
 # Inyectar CSS en la aplicación
-st.markdown(hide_github_style, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # --- 1. CONFIGURACIÓN DE PÁGINA ---
 st.set_page_config(page_title="Supervisión Despacho - SEIN", layout="wide", initial_sidebar_state="expanded")
